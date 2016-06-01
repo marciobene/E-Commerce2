8a34231d66d985a4002966c69a6f511d58827e7a
Marcio Benetasso <marciobene@gmail.com>
2016-06-01 10:46:17 -0300

    Novo Projeto inicial sem bugs

---
+++ src/java/beans/ProdutoBean.java
@@ -0,0 +1,26 @@
+package beans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author MarcioBene
+ */
+@ManagedBean
+@RequestScoped
+public class ProdutoBean {
+
+    /**
+     * Creates a new instance of produtoBean
+     */
+    public ProdutoBean() {
+    }
+    
+}
